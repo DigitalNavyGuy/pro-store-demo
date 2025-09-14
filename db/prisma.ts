1a5ba73edb09d49cd6b05535ce8abc5536706c29
John Fuller <fuller.john84@gmail.com>
2025-09-14 11:56:01 -0400

    Setup serverless environment config.

---
+++ db/prisma.ts
@@ -0,0 +1,23 @@
+import { PrismaNeon } from "@prisma/adapter-neon";
+import { PrismaClient } from "../lib/generated/prisma";
+
+// Instantiates the Prisma adapter using the Neon connection pool to handle the connection between Prisma and Neon.
+const adapter = new PrismaNeon({ connectionString: process.env.DATABASE_URL! });
+
+// Extends the PrismaClient with a custom result transformer to convert the price and rating fields to strings.
+export const prisma = new PrismaClient({ adapter }).$extends({
+  result: {
+    product: {
+      price: {
+        compute(product) {
+          return product.price.toString();
+        },
+      },
+      rating: {
+        compute(product) {
+          return product.rating.toString();
+        },
+      },
+    },
+  },
+});
