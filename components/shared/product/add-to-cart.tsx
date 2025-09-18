44e2de1a9d99aa6986a65b4d0437940b2524072b
John Fuller <fuller.john84@gmail.com>
2025-09-17 22:38:09 -0400

    Added Smooth UI w/ useTransition hook.

--- components/shared/product/add-to-cart.tsx
+++ components/shared/product/add-to-cart.tsx
@@ -2,48 +2,56 @@
 
 import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
-import { Minus, Plus } from "lucide-react";
+import { Loader, Minus, Plus } from "lucide-react";
 import type { Cart, CartItem } from "@/types";
 import { toast } from "sonner";
 import { addItemToCart, removeItemFromCart } from "@/lib/actions/cart.actions";
+import { useTransition } from "react";
 
 const AddToCart = ({ cart, item }: { cart?: Cart; item: CartItem }) => {
   const router = useRouter();
 
+  const [isPending, startTransition] = useTransition();
+
   const handleAddToCart = async () => {
-    const res = await addItemToCart(item);
+    startTransition(async () => {
+      const res = await addItemToCart(item);
 
-    if (!(res?.success ?? false)) {
-      toast.error(res!.message, {
-        style: { backgroundColor: "red", color: "white" },
+      if (!(res?.success ?? false)) {
+        toast.error(res!.message, {
+          style: { backgroundColor: "red", color: "white" },
+        });
+        return;
+      }
+
+      // Handle successful add to cart
+      toast.success(res.message, {
+        style: { backgroundColor: "green", color: "white" },
+        action: {
+          label: "Go to Cart",
+          onClick: () => router.push("/cart"),
+        },
       });
-      return;
-    }
-
-    // Handle successful add to cart
-    toast.success(res.message, {
-      style: { backgroundColor: "green", color: "white" },
-      action: {
-        label: "Go to Cart",
-        onClick: () => router.push("/cart"),
-      },
     });
   };
 
   // Handle remove from cart
   const handleRemoveFromCart = async () => {
-    const res = await removeItemFromCart(item.product);
-    const message =
-      res?.message ?? (res?.success ? "Cart updated" : "Something went wrong");
+    startTransition(async () => {
+      const res = await removeItemFromCart(item.product);
+      const message =
+        res?.message ??
+        (res?.success ? "Cart updated" : "Something went wrong");
 
-    toast(message, {
-      style: {
-        backgroundColor: res?.success ? "green" : "red",
-        color: "white",
-      },
+      toast(message, {
+        style: {
+          backgroundColor: res?.success ? "green" : "red",
+          color: "white",
+        },
+      });
+
+      return;
     });
-
-    return;
   };
   // Check if the item is already in the cart
   const existItem = cart && cart.items.find((x) => x.product === item.product);
@@ -51,16 +59,29 @@ const AddToCart = ({ cart, item }: { cart?: Cart; item: CartItem }) => {
   return existItem ? (
     <div>
       <Button type="button" variant="outline" onClick={handleRemoveFromCart}>
-        <Minus className="h-4 w-4" />
+        {isPending ? (
+          <Loader className="h-4 w-4 animate-spin" />
+        ) : (
+          <Minus className="h-4 w-4" />
+        )}
       </Button>
       <span className="px-2">{existItem.qty}</span>
       <Button type="button" variant="outline" onClick={handleAddToCart}>
-        <Plus className="h-4 w-4" />
+        {isPending ? (
+          <Loader className="h-4 w-4 animate-spin" />
+        ) : (
+          <Plus className="h-4 w-4" />
+        )}
       </Button>
     </div>
   ) : (
     <Button className="w-full" type="button" onClick={handleAddToCart}>
-      <Plus /> Add to Cart
+      {isPending ? (
+        <Loader className="h-4 w-4 animate-spin" />
+      ) : (
+        <Plus className="h-4 w-4" />
+      )}
+      Add to Cart
     </Button>
   );
 };
