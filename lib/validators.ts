9df3d4368718123fadb07f7e66cc38ca1371ba83
John Fuller <fuller.john84@gmail.com>
2025-09-22 17:24:17 -0400

    Created Order action.

--- lib/validators.ts
+++ lib/validators.ts
@@ -104,7 +104,7 @@ export const insertOrderSchema = z.object({
   userId: z.string().min(1, "User is required"),
   itemsPrice: currency,
   shippingPrice: currency,
-  tacPrice: currency,
+  taxPrice: currency,
   totalPrice: currency,
   paymentMethod: z.string().refine((data) => PAYMENT_METHODS.includes(data), {
     message: "Invalid payment method",
