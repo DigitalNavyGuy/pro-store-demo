4860626a85ecefc16bc8923e6752e902e2c83006
John Fuller <fuller.john84@gmail.com>
2025-09-22 23:50:33 -0400

    Added ConfirmationDetailsTable.

--- app/(root)/order/[id]/page.tsx
+++ app/(root)/order/[id]/page.tsx
@@ -2,6 +2,7 @@ import { Metadata } from "next";
 import { getOrderById } from "@/lib/actions/order.actions";
 import { notFound } from "next/navigation";
 import { ShippingAddress } from "@/types";
+import ConfirmationDetailsTable from "./order-confirmation-details";
 
 export const metadata: Metadata = {
   title: "Order Details",
@@ -17,7 +18,14 @@ const OrderDetailsPage = async (props: {
   const order = await getOrderById(id);
   if (!order) notFound();
 
-  return <>Order Details</>;
+  return (
+    <ConfirmationDetailsTable
+      order={{
+        ...order,
+        shippingAddress: order.shippingAddress as ShippingAddress,
+      }}
+    />
+  );
 };
 
 export default OrderDetailsPage;
